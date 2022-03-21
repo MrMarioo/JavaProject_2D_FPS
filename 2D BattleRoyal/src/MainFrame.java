d3725493e63a698da82ef2a97068c53da5289076
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2022-03-21 19:44:27 +0100

    Add files via upload

--- 2D BattleRoyal/src/MainFrame.java
+++ 2D BattleRoyal/src/MainFrame.java
@@ -7,9 +7,13 @@ public class MainFrame extends JFrame implements ActionListener{
 	// labels buttons
 	private JButton playButton;
 	private JTextField nickText;
+	private JTextField serverText;
 	private JPanel panel;
 	private JLabel nickLabel;
+	private JLabel serverLabel;
+	private JLabel imageLabel;
 	private String playerNick;
+	private String server;
 	MainFrame()
 	{
 		this.setTitle("2D BATTLEROYALE");
@@ -18,19 +22,30 @@ public class MainFrame extends JFrame implements ActionListener{
 		this.setSize(420,420);
 		
 		ImageIcon image = new ImageIcon("pngegg.png");
+		ImageIcon rifles = new ImageIcon("rifles.png");
 		this.setIconImage(image.getImage());
 		this.getContentPane().setBackground(new Color(0x344a8c));
 		nickLabel = new JLabel("Put your nick");
 		nickLabel.setVerticalTextPosition(JLabel.TOP);
 		nickLabel.setHorizontalTextPosition(JLabel.CENTER);
+		serverLabel = new JLabel("Put your server");
+		serverLabel.setVerticalTextPosition(JLabel.TOP);
+		serverLabel.setHorizontalTextPosition(JLabel.CENTER);
+		imageLabel = new JLabel();
+		imageLabel.setIcon(rifles);
 		playButton = new JButton("Play");
 		playButton.addActionListener(this);
+		
 		nickText = new JTextField(16);
+		serverText = new JTextField(16);
 		panel = new JPanel();
 		panel.setBounds(0,0,420,200);
 		panel.add(nickText);
-		panel.add(playButton);
 		panel.add(nickLabel);
+		panel.add(serverText);
+		panel.add(serverLabel);
+		panel.add(playButton);
+		panel.add(imageLabel);
 		panel.setVisible(true);
 		
 		this.add(panel);
@@ -40,13 +55,19 @@ public class MainFrame extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==playButton) {
 			playerNick=nickText.getText();
+			server=serverText.getText();
 			System.out.println(playerNick);
+			System.out.println(server);
 			nickLabel.setVisible(true);
 		}	
 	}
 	public String getNick()
 	{
 		return playerNick;
+	}
+	public String getServer()
+	{
+		return server;
 	}
 	
 }
